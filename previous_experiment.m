637fecb12611db90dd34aae33eacbf56c116bb3d
Maria <mariia.v.litvinova@gmail.com>
2016-10-17 00:58:37 +0300

    added results for different noises

--- previous_experiment.m
+++ previous_experiment.m
@@ -1,31 +1,27 @@
-function [previous_experiment_result, x1_1_vector, x1_2_vector, x2_1_vector,x2_2_vector, y1_vector, y2_vector, delta_vector] = previous_experiment(a, b, c, d, fun)
-    result = zeros(1, 500);
-
+function [previous_experiment_result, x1_1_vector, x1_2_vector, x2_1_vector,x2_2_vector, y1_vector, y2_vector, delta_vector] = previous_experiment(a, b, c, v)
+    number_of_iterations = 90;
+    
     u = 20;
     k = 1;
     
-    x1_first = zeros(1, 500);
-    x1_second = zeros(1, 500);
+    result = zeros(1, number_of_iterations);
     
-    x2_first = zeros(1, 500);
-    x2_second = zeros(1, 500);
+    x1_first = zeros(1, number_of_iterations);
+    x1_second = zeros(1, number_of_iterations);
     
-    y_first = zeros(1, 500);
-    y_second = zeros(1, 500);
+    x2_first = zeros(1, number_of_iterations);
+    x2_second = zeros(1, number_of_iterations);
     
-    deltas = zeros(1, 500);
+    y_first = zeros(1, number_of_iterations);
+    y_second = zeros(1, number_of_iterations);
     
-    while k <= 500
+    deltas = zeros(1, number_of_iterations);
+    
+    while k <= number_of_iterations
         delta = generate_delta();
         
         beta = 10;
-        alpha = 3;
-        
-        v1 = 0;%2 * (rand() - 0.5);
-        v2 = 0;%2 * (rand() - 0.5);
-        
-        %v1 = -2;
-        %v2 = -2;
+        alpha = 2;
         
         b_uu = max(u + beta * delta, 10);
         
@@ -35,19 +31,25 @@ function [previous_experiment_result, x1_1_vector, x1_2_vector, x2_1_vector,x2_2
         if ((x1 > 0) && (x12 > 0))
             k = k + 1;
             
+            if v == 0 
+                v_real = 2 * (rand() - 0.5);
+            else
+                v_real = v;
+            end
+            
             deltas(k) = delta;
             
             x1_first(k) = x1;
             x1_second(k) = x12;
         
             if (x1 > 0)
-                x2 = max(b - exp(x1 - c), 0);
+                x2 = max(b - exp(x1 - c), -200);
             else
                 x2 = 0;
             end
             
             if (x12 > 0)
-                x222 = max(b - exp(x12 - c), 0);
+                x222 = max(b - exp(x12 - c), -200);
             else
                 x222 = 0;
             end
@@ -55,8 +57,8 @@ function [previous_experiment_result, x1_1_vector, x1_2_vector, x2_1_vector,x2_2
             x2_first(k) = x2;
             x2_second(k) = x222;
             
-            yk1 = -max(x1 * x2, 0)+ v1;
-            yk2 = -max(x12 * x222, 0) + v2;
+            yk1 = -max(x1 * x2, -200)+ v_real;
+            yk2 = -max(x12 * x222, -200) + v_real;
             
             y_first(k) = yk1;
             y_second(k) = yk2;
--- spsa.m
+++ spsa.m
@@ -11,13 +11,25 @@ d = 0.5;
 figure
 plot(x, plot_result);
 
+v1 = 0;
+v2 = 2;
+v3 = -2;
 
-[experiment_result, x11, x12, x21, x22, y1, y2, delta] = previous_experiment(a, b, c, d, evaluation_functional_1);
-T = table(delta.', x11.', x21.', y1.', x12.', x22.', y2.', experiment_result.');
-writetable(T, 'results.xls');
+[experiment_result1, x11, x12, x21, x22, y1, y2, delta] = previous_experiment(a, b, c, v1);
+T = table(delta.', x11.', x21.', y1.', x12.', x22.', y2.', experiment_result1.');
+writetable(T, 'results_for_standart_noise.xls');
 figure
-plot(experiment_result);
+plot(experiment_result1, 'color', 'r'); hold on;
 
+[experiment_result2, x11, x12, x21, x22, y1, y2, delta] = previous_experiment(a, b, c, v2);
+T = table(delta.', x11.', x21.', y1.', x12.', x22.', y2.', experiment_result2.');
+writetable(T, 'results_for_positive_noise.xls');
+plot(experiment_result2, 'color', 'g'); hold on;
+
+[experiment_result3, x11, x12, x21, x22, y1, y2, delta] = previous_experiment(a, b, c, v3);
+T = table(delta.', x11.', x21.', y1.', x12.', x22.', y2.', experiment_result3.');
+writetable(T, 'results_for_negative_noise.xls');
+plot(experiment_result3, 'color', 'b');
 %{
 
 plot_result = plot_function(a, b, c, d, evaluation_functional_1);
